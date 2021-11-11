1978f03369ecb9fd620dfaaf2e338ee9161e0860
txyuqiwang <txyuqiwang@tencent.com>
2021-11-11 14:10:15 +0800

    添加hadoop的运行文件

---
+++ hadoop/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cur_dir=`pwd`
+
+
+docker stop node1
+docker rm node1
+docker stop node2
+docker rm node2
+
+docker network create --subnet=172.18.0.0/16 mynetwork
+
+
+docker run --name='node1' \
+      -itd --privileged  \
+      --net mynetwork \
+      --ip 172.18.0.2 \
+	  -p 50070:50070 \
+	  -p 8088:8088 \
+	  wyqwb/wyq-base-centos	  
+	  
